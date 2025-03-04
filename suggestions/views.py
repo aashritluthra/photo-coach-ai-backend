cc2e847876cdf5a91db538a8e557593e505435be
Aashrit Luthra <aashritluthra01@gmail.com>
2025-03-03 21:26:17 -0800

    Add a todo for GET method implementation

--- suggestions/views.py
+++ suggestions/views.py
@@ -2,5 +2,7 @@ from django.http import JsonResponse
 from django.views import View
 
 class SuggestionsView(View):
+    # TODO: Implement the get method to retrieve request 
+    # parameters and fetch suggestions from a LLM call.
     def get(self, request):
         return JsonResponse({"message": "GET request received!"})
